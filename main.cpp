d8dd76ddbbee2616bb010070c22010dd47979f9e
Mark Alitawi <malitawi@gmail.com>
2018-09-22 17:30:28 -0700

    updates

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <memory>
+
+using std::string;
+using std::ifstream; using std::getline;
+using std::shared_ptr; using std::make_shared;
+using std::cerr; using endl;
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
