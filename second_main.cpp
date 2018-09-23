d902b1596fd5b4b95ccfb4ba8e590710880d5571
Mark Alitawi <malitawi@gmail.com>
2018-09-22 18:58:42 -0700

    adding second file

---
+++ second_main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <memory>
+
+using std::string;
+using std::ifstream; using std::getline;
+using std::shared_ptr; using std::make_shared;
+using std::cerr; using endl;
+
+int main(int argc, char* argv[])
+{
+    cout << "foo";
+    cout << "bar";
+    cout << "baz";
+    
+    return 0;
+}
